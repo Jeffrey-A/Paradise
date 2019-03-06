305a47980a59ff8ffa0acc058e5776dc966f1eee
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2019-03-06 08:51:19 -0500

    main nav modified

--- js/script.js
+++ js/script.js
@@ -1,26 +1,32 @@
-const menuButton = document.querySelector("#menu-icon-container");
-const ul =  document.querySelector("ul");
-const menuIcon =  document.querySelector("#menu-icon-container img");
-const close = document.createElement("P");
-close.textContent ="X"
-close.className = "closeMenu";
+const $menuButton = $("#menu-icon-container");
+const $ul =  $("#main-nav ul");
+const $menuIcon = $("#menu-icon-container img");
+const $close = $("<p></p>");
+$close.text("X")
+$close.addClass("closeMenu");
 
 
+
+function closeMenu(){
+    $close.remove();
+    $menuButton.append($menuIcon);
+    $ul.slideUp('slow');
+}
+
+var temp = true;
 function expandMenu(){
-    if( menuButton.firstChild === menuIcon){
-        menuButton.removeChild(menuIcon);
-        menuButton.appendChild(close);
-        ul.style.display = "block";
+    if(temp){
+        $menuIcon.remove();
+        $menuButton.append($close);
+        $ul.slideDown('slow');
+        temp = false;
     }else{
         closeMenu();
+        temp=true;
     }
 
 }
 
-function closeMenu(){
-    menuButton.removeChild(close);
-    menuButton.appendChild(menuIcon);
-    ul.style.display ="none";
-}
 
-menuButton.addEventListener('click', expandMenu);
+
+$menuButton.click(expandMenu);
