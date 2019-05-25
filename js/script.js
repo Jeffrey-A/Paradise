c2b713070ed4f73e5f7f625bb1dd4b50fd0e31f9
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2019-05-24 23:27:37 -0400

    nav removed and bootstrap added

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
-const $menuButton = $("#menu-icon-container");
-const $ul =  $("#main-nav ul");
-const $menuIcon = $("#menu-icon-container img");
-const $close = $("<p></p>");
-$close.text("X")
-$close.addClass("closeMenu");
+// const $menuButton = $("#menu-icon-container");
+// const $ul =  $("#main-nav ul");
+// const $menuIcon = $("#menu-icon-container img");
+// const $close = $("<p></p>");
+// $close.text("X")
+// $close.addClass("closeMenu");
 
 
 
-function closeMenu(){
-    $close.remove();
-    $menuButton.append($menuIcon);
-    $ul.slideUp('slow');
-}
+// function closeMenu(){
+//     $close.remove();
+//     $menuButton.append($menuIcon);
+//     $ul.slideUp('slow');
+// }
 
-var temp = true;
-function expandMenu(){
-    if(temp){
-        $menuIcon.remove();
-        $menuButton.append($close);
-        $ul.slideDown('slow');
-        temp = false;
-    }else{
-        closeMenu();
-        temp=true;
-    }
+// var temp = true;
+// function expandMenu(){
+//     if(temp){
+//         $menuIcon.remove();
+//         $menuButton.append($close);
+//         $ul.slideDown('slow');
+//         temp = false;
+//     }else{
+//         closeMenu();
+//         temp=true;
+//     }
 
-}
+// }
 
-$menuButton.click(expandMenu);
+// $menuButton.click(expandMenu);
